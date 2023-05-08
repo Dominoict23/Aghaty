561e099fdb2d0a99d5b2570c2496f41ea3b92cb5
IbrahemAtef <ibrahim.nemer.1999@gmail.com>
2023-05-08 13:30:55 +0300

    edit db con

--- db/config/connection.js
+++ db/config/connection.js
@@ -4,9 +4,19 @@ const { Sequelize } = require("sequelize");
 dotenv.config();
 const { NODE_ENV, DATABASE_URL, DEV_DATABASE_URL } = process.env;
 
-const sequelize = new Sequelize("aghaty", "root", "root", {
-  dialect: "mysql",
-  host: "localhost",
-});
+// const sequelize = new Sequelize("aghaty", "root", "root", {
+//   dialect: "mysql",
+//   host: "localhost",
+// });
+
+const sequelize = new Sequelize(
+  "globegki_aghaty",
+  "globegki_root",
+  "059283805928388",
+  {
+    dialect: "mysql",
+    host: "localhost",
+  }
+);
 
 module.exports = sequelize;
